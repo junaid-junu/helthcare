23c07197056949c9038faaaa5ba55f089ff7182a
junaid <muhammedjunaid993@gmail.com>
2026-02-27 11:17:54 +0530

    som edited

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use('/api/prescriptions', require('./src/routes/prescriptionRoutes'));
 app.use('/api/appointments', require('./src/routes/appointmentRoutes'));
 
 // Database Connection
-mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/healthcare')
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log('MongoDB connection error:', err));
 
