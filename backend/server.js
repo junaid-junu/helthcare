cc9df65307c20845a014e397f3ef0bda32c0eeb1
junaid <muhammedjunaid993@gmail.com>
2026-02-28 17:10:28 +0530

    som uption

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:3001'], // 3000 for doctor, 3001 for patient
-    credentials: true,
-}));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
